e3af1fd653dd9e0829f0e0f7cbafa30079ffdfe2
Jason Benaim <jkbenaim@gmail.com>
2012-10-22 18:34:10 -0400

    Added support for the Game Boy Camera.

--- source/mbc_cam.c
+++ source/mbc_cam.c
@@ -16,18 +16,17 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
 
+// TODO: actually save the cart ram to disk.
+
 #include "memory.h"
 #include "cart.h"
 #include "mbc_cam.h"
 #include <stdio.h>
 
-u8 cam_extram[32768]; //overkill
+u8 cam_extram[131072];
 
-
-u8 cam_rom_bank_low; // bits 0-4
-u8 cam_rom_bank_high;  // bit 5-6 (stored in bits 0-1)
 u8 cam_ram_bank;
-u8 cam_mode_select;
+int cam_mode;   // 0=RAM accessible, 1=CAM registers accessible
 
 void mbc_cam_install()
 {
@@ -41,7 +40,7 @@ void mbc_cam_install()
     readmem[i] = mbc_cam_read_bank_n;
   }
   
-  // write 0000-1FFF: ram enable
+  // write 0000-1FFF: ram write enable
   for( i=0x00; i<=0x1F; ++i ) {
     writemem[i] = mbc_cam_write_ram_enable;
   }
@@ -49,20 +48,20 @@ void mbc_cam_install()
   for( i=0x20; i<=0x3F; ++i ) {
     writemem[i] = mbc_cam_write_rom_bank_select;
   }
-  // write 4000-5FFF: ram bank select
+  // write 4000-5FFF: extram bank/mode select
   for( i=0x40; i<=0x5F; ++i ) {
-    writemem[i] = mbc_cam_write_ram_bank_select;
+    writemem[i] = mbc_cam_extram_bank_select;
   }
-  // write 6000-7FFF: mode select
+  // write 6000-7FFF: nothing
   for( i=0x60; i<=0x7F; ++i ) {
-    writemem[i] = mbc_cam_write_mode_select;
+    writemem[i] = mbc_cam_dummy;
   }
   
-  // read A000-BFFF: read extram/timer
+  // read A000-BFFF: read extram
   for( i=0xA0; i<=0xBF; ++i ) {
     readmem[i] = mbc_cam_read_extram;
   }
-  // write A000-BFFF: write extram/timer
+  // write A000-BFFF: write extram
   for( i=0xA0; i<=0xBF; ++i ) {
     writemem[i] = mbc_cam_write_extram;
   }
@@ -70,35 +69,14 @@ void mbc_cam_install()
   // set up cart params
   cart.cartrom_bank_zero = cart.cartrom;
   cart.cartrom_bank_n = cart.cartrom + 0x4000;
-  cam_rom_bank_low = 0x01;
-  cam_rom_bank_high = 0x00;
   cam_ram_bank = 0x00;
-  cam_mode_select = 0x00;
+  cam_mode = 0;
   cart.extram = cam_extram;
-  cart.extram_bank = cam_extram;
-}
-
-void mbc_cam_regs_changed()
-{
-  int rambank;
-  int rombank = cam_rom_bank_low;
-  if( rombank == 0 )
-    rombank++;
   
-  if( cam_mode_select == 0 )
-  {
-    // ROM mode
-    rombank += (cam_rom_bank_high & 0x03) << 5;
-    rambank = 0;
-  } else {
-    // RAM mode
-    rambank = cam_rom_bank_high & 0x03;
-  }
-  
-  size_t cartoffset;
-  cartoffset = (rombank*16384) % cart.cartromsize;
-  cart.cartrom_bank_n = cart.cartrom + cartoffset;
-  cart.extram_bank = cart.extram + rambank * 8192;
+  // clear extram
+  int index;
+  for( index=0; index<131072; index++ )
+    cam_extram[index] = 0x41;
 }
 
 void mbc_cam_dummy()
@@ -116,32 +94,80 @@ void mbc_cam_read_bank_n() {
 
 // write 0000-1FFF
 void mbc_cam_write_ram_enable() {
+  // TODO
 }
 
 // write 2000-3FFF
 void mbc_cam_write_rom_bank_select() {
-  cam_rom_bank_low = memByte;
-  mbc_cam_regs_changed();
+  size_t cartoffset;
+  int rombank = (memByte==0)?1:memByte;
+  cartoffset = (rombank*16384) % cart.cartromsize;
+  cart.cartrom_bank_n = cart.cartrom + cartoffset;
 }
 
 // write 4000-5FFF
-void mbc_cam_write_ram_bank_select() {
-  cam_rom_bank_high = memByte;
-  mbc_cam_regs_changed();
+void mbc_cam_extram_bank_select() {
+  if( memByte == 0x10 )
+  {
+    // CAM mode
+    cam_mode = 1;
+  }
+  else
+  {
+    // RAM mode
+    // access RAM like normal
+    cam_mode = 0;
+    int bank = memByte & 0x0F;
+    cart.extram = cam_extram + bank*8192;
+  }
+  
 }
 
 // write 6000-7FFF
 void mbc_cam_write_mode_select() {
-  cam_mode_select = memByte;
-  mbc_cam_regs_changed();
 }
 
 // read A000-BFFF
 void mbc_cam_read_extram() {
-  memByte = cart.extram_bank[address&0x1fff];
+  if( cam_mode == 0 )
+  {
+    // access RAM like normal
+    memByte = cart.extram[address&0x1fff];
+  }
+  else
+  {
+    // access CAM registers
+    memByte = 0; //TODO
+  }
 }
 
+// int pics_taken = 0;
 // write A000-BFFF
 void mbc_cam_write_extram() {
-  cart.extram_bank[address&0x1fff] = memByte;
+  if( cam_mode == 0 )
+  {
+    // access RAM like normal
+    cart.extram[address&0x1fff] = memByte;
+  }
+  else
+  {
+    // access CAM registers
+    switch( address )
+    {
+      case 0xA000:
+        // take a picture?
+        switch( memByte )
+        {
+          case 0x03:
+            // really take that picture
+//             printf( "Picture taken! [%d]\n", pics_taken++ );
+            break;
+          default:
+            break;
+        }
+        break;
+      default:
+        break;
+    }
+  }
 }
