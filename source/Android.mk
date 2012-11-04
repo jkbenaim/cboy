2acd69f016868535a438b636f9e100afae2d5016
Jason Benaim <jkbenaim@gmail.com>
2012-11-03 22:43:51 -0400

    Now building for release.

--- source/Android.mk
+++ source/Android.mk
@@ -5,6 +5,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := cboy
 LOCAL_SRC_FILES := cart.c cpu.c input.c main.c mbc_boot.c mbc.c mbc_cam.c mbc_mbc1.c mbc_mbc2.c mbc_mbc3.c mbc_mbc5.c mbc_none.c memory.c video.c
 LOCAL_LDLIBS    := -llog -ljnigraphics
-LOCAL_CFLAGS    := -O3
 
 include $(BUILD_SHARED_LIBRARY)
--- source/Application.mk
+++ source/Application.mk
@@ -1,2 +1,3 @@
 APP_ABI := armeabi armeabi-v7a
 APP_PLATFORM := android-8
+APP_OPTIM := release
