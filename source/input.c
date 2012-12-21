eabf7deef886619ba94ad5ae8a0295be7a647ded
Jason Benaim <jkbenaim@gmail.com>
2012-12-21 01:17:08 -0500

    Added dummy input stuff for Android.

--- source/input.c
+++ source/input.c
@@ -16,6 +16,10 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
 
+#ifdef __ANDROID__
+#include <jni.h>
+#endif // __ANDROID__
+
 #include "types.h"
 #include "main.h"
 #include "memory.h"
@@ -136,6 +140,22 @@ int input_event_filter(const SDL_Event *event)
   return 0;
 }
 #endif // USE_SDL
+
+#ifdef __ANDROID__
+JNIEXPORT void JNICALL Java_org_trashfolder_cboy_CboyView_cboyPressAButton(JNIEnv * env)
+{
+  state.joyp_buttons &= ~INPUT_BUTTONS_A;
+  state.iflag |= IMASK_JOYPAD;
+  state.halt = 0;
+}
+
+JNIEXPORT void JNICALL Java_org_trashfolder_cboy_CboyView_cboyUnpressAButton(JNIEnv * env)
+{
+  state.joyp_buttons |= INPUT_BUTTONS_A;
+  state.iflag |= IMASK_JOYPAD;
+  state.halt = 0;
+}
+#endif // __ANDROID__
 
 void input_init()
 {
