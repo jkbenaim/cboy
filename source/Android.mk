b987c3c0bf503b076647a80520849f9470f92eca
Jason Benaim <jkbenaim@gmail.com>
2013-05-09 22:57:33 -0400

    Initial mbc7 support.

--- source/Android.mk
+++ source/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := cboy
-LOCAL_SRC_FILES := cart.c cpu.c input.c main.c mbc_boot.c mbc.c mbc_cam.c mbc_mbc1.c mbc_mbc2.c mbc_mbc3.c mbc_mbc5.c mbc_none.c memory.c serial.c video.c
+LOCAL_SRC_FILES := cart.c cpu.c input.c main.c mbc_boot.c mbc.c mbc_cam.c mbc_mbc1.c mbc_mbc2.c mbc_mbc3.c mbc_mbc5.c mbc_mbc7.c mbc_none.c memory.c serial.c video.c
 LOCAL_LDLIBS    := -llog -ljnigraphics
 
 include $(BUILD_SHARED_LIBRARY)
