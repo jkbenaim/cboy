68a997aae61d840c3f6b629db5f4f25c2733011e
Jason Benaim <jkbenaim@gmail.com>
2017-05-09 04:27:54 -0400

    I guess we need this now?

--- src/cart_chardev.c
+++ src/cart_chardev.c
@@ -16,6 +16,8 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
 
+#define _GNU_SOURCE 1
+
 #include "cart_chardev.h"
 #include "cart.h"
 #include "main.h"
