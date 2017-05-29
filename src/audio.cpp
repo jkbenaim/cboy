06164c04e8b1a30ce40f503d91dbda0461f6e268
Jason Benaim <jkbenaim@gmail.com>
2017-05-29 01:58:36 -0400

    Audio: now works.

--- src/audio.cpp
+++ src/audio.cpp
@@ -2,28 +2,50 @@
 #include "Basic_Gb_Apu.h"
 #include "Sound_Queue.h"
 #include "audio.h"
-
+#include <stdio.h>
 #define debugprintf printf
 #define debugprintf(...)
 
 Basic_Gb_Apu *apu;
-//Sound_Queue sound;
 
-const int sample_rate = 44100;
+const int sample_rate = 48000;
+void fill_audio( void *userdata, uint8_t *stream, int len );
 
 int audio_init()
 {
     debugprintf("audio was inited\n");
     apu = new Basic_Gb_Apu();
     apu->set_sample_rate( sample_rate );
-    //sound.start( sample_rate, 2 );
+
+    SDL_AudioSpec wanted;
+    wanted.freq = sample_rate;
+    wanted.format = AUDIO_S16SYS;
+    wanted.channels = 2;
+    wanted.samples = 1024;
+    wanted.callback = fill_audio;
+    wanted.userdata = NULL;
+
+    if( SDL_OpenAudio( &wanted, NULL ) < 0 )
+    {
+        fprintf( stderr, "Couldn't open audio: %s\n", SDL_GetError() );
+        return 1;
+    }
+    SDL_PauseAudio(0);
     return 0;
+}
+
+void fill_audio( void *userdata, uint8_t *stream, int len )
+{
+    debugprintf( "fill_audio( %p, %p, %d );\n", userdata, stream, len );
+    memset( stream, 0, len );
+    long count = apu->read_samples( (blip_sample_t *)stream, len/2 );
+    debugprintf( "%d\n", count );
 }
 
 void audio_cleanup()
 {
     debugprintf("audio was cleant up");
-    //sound.stop();
+    SDL_CloseAudio();
 }
 
 uint8_t audio_read( uint16_t address )
@@ -39,9 +61,5 @@ void audio_write( uint16_t address, uint8_t data )
 void audio_frame()
 {
     apu->end_frame();
-    const int buf_size = 20480;
-    static blip_sample_t buf[buf_size];
-    long count = apu->read_samples( buf, buf_size );
-    //sound.write( buf, count );
 }
 
