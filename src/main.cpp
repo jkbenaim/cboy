444fbaef25152e1cff6fe8220711bf9a8fce08ef
Jason Benaim <jkbenaim@gmail.com>
2017-06-07 17:45:50 -0400

    main.cpp: add command system.

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,6 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
 
-#include <stdlib.h>
 #include "types.h"
 #include "input.h"
 #include "cpu.h"
@@ -30,42 +29,153 @@
 int stop = 0;
 int pause = 0;
 
-int main ( int argc, char* argv[] ) {
-  printf( " ----------------------- \n" );
-  printf( " --- Welcome to cboy --- \n" );
-  printf( " ---  (c) 2012-2017  --- \n" );
-  printf( " ----------------------- \n" );
-  
-  if(argc < 2)
-  {
-    fprintf(stderr, "Usage: cboy game.gb\n");
-    exit(1);
-  }
-  
-  mem_init();
-  audio_init();
-  if( ! cpu_init() )
-  {
-    fprintf( stderr, "cpu init failed\n" );
-    return 1;
-  }
-  cart_init( NULL, argv[1] );
-  vid_init();
-  input_init();
-  while(!stop)
-  {
-    vid_waitForNextFrame();
-    input_handle();
-    if(!pause)
+struct command {
+    const char *name;
+    int (*func)(int, char**);
+    const char *help_short;
+    const char *help_long;
+    int end;    // 0 (default) for commands, 1 for end-of-array sentinel
+};
+void print_usage();
+int cmd_run(int, char**);
+int cmd_help(int, char**);
+int cmd_exit(int, char**);
+
+struct command cmds[] = {
     {
-      cpu_do_one_frame();
-      vid_frame();
-      audio_frame();
+        .name = "help",
+        .func = cmd_help,
+        .help_short = "Get help for a command.",
+        .help_long = "help <command>\n\tget help for a command",
+    },
+    {
+        .name = "run",
+        .func = cmd_run,
+        .help_short = "Run a Game Boy ROM in the emulator.",
+        .help_long = "run <romfile>\n\trun romfile in the Game Boy emulator",
+    },
+    {
+        .name = "",
+        .func = cmd_exit,
+        .help_short = "",
+        .help_long = "",
+        .end = 1, // end-of-array sentinel
+    },
+};
+
+int get_cmd_index_for_name( char *needle )
+{
+    int cmds_index = 0;
+    while( !cmds[cmds_index].end )
+    {
+        if( !strcmp( cmds[cmds_index].name, needle ) )
+        {
+            return cmds_index;
+        }
+        cmds_index++;
     }
-  }
-  
-  cart_cleanup();
-   audio_cleanup();
-  
-  return 0;
+
+    return -1;
+}
+
+int main( int argc, char *argv[] )
+{
+    if( argc < 2 )
+    {
+        print_usage();
+        return 1;
+    }
+
+    char *exe_name = argv[0];
+    char *command_name = argv[1];
+    int cmd_index = get_cmd_index_for_name( command_name );
+    if( cmd_index == -1 )
+    {
+        printf( "%s: '%s' is not a command.\n", exe_name, command_name );
+        return 1;
+    }
+    else
+    {
+        return cmds[cmd_index].func( argc, argv );
+    }
+}
+
+void print_usage()
+{
+    printf( "usage: cboy <command> <[args]>\n"
+            "\n"
+            "The available commands are:\n" );
+    int cmds_index=0;
+    while( !cmds[cmds_index].end )
+    {
+        printf( "\t%s\t%s\n", cmds[cmds_index].name, cmds[cmds_index].help_short);
+        cmds_index++;
+    }
+
+    printf( "\n"
+            "See 'cboy help <command>' to read about a specific command.\n" );
+}
+
+int cmd_help( int argc, char *argv[] )
+{
+    if( argc < 3 )
+    {
+        fprintf( stderr, "usage: cboy help <command>\n" );
+        return 1;
+    }
+
+    char *command_name = argv[2];
+    int cmds_index=0;
+    while( !cmds[cmds_index].end )
+    {
+        if( !strcmp( cmds[cmds_index].name, command_name ) )
+        {
+            printf( "%s\n", cmds[cmds_index].help_long );
+            return 0;
+        }
+        cmds_index++;
+    }
+    printf( "No command %s\n", command_name );
+    return 1;
+}
+
+int cmd_run( int argc, char *argv[] )
+{
+    if( argc < 3 )
+    {
+        fprintf( stderr, "usage: cboy run <romfile>\n" );
+        return 1;
+    }
+
+    mem_init();
+    audio_init();
+    if( !cpu_init() )
+    {
+        fprintf( stderr, "cpu init failed\n" );
+        return 1;
+    }
+    cart_init( NULL, argv[2] );
+    vid_init();
+    input_init();
+    while( !stop )
+    {
+        vid_waitForNextFrame();
+        input_handle();
+        if( !pause )
+        {
+            cpu_do_one_frame();
+            vid_frame();
+            audio_frame();
+        }
+    }
+
+    cart_cleanup();
+    audio_cleanup();
+
+    return 0;
+}
+
+int cmd_exit( int argc, char *argv[] )
+{
+    return 0;
 }
